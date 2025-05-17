e90b6fcebc7b8f361cc71f5e7ade023e5627316f
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-05-17 18:04:54 +0500

    fix

--- backend/app/router/auth.py
+++ backend/app/router/auth.py
@@ -1,1 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+
+from app.database.models.users import User
+from app.schemas.auth import UserLoginRequest, UserRegisRequest
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Auth"]
+)
+
+
+
