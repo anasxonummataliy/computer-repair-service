e91abb55d2e0996ef09b1d2602d61757af0892fc
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-05-24 10:10:31 +0500

    fix

--- backend/app/router/auth.py
+++ backend/app/router/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
 
 from app.database.models.users import User
 from app.schemas.auth import UserLoginRequest, UserRegisRequest
@@ -10,12 +11,31 @@ router = APIRouter(
     tags=["Auth"]
 )
 
-@router.get('')
+@router.get('/register')
 async def registration(
     user : UserRegisRequest,
     db : AsyncSession = Depends(get_db)
 ):
     pass
 
+
+@router.post('/login')
+async def login(
+    user : UserLoginRequest,
+    db : AsyncSession = Depends(get_db)
+):
+    smtm = select(User).where(User.email == user.email)
+    result = await db.execute(smtm)
+    if not smtm:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not smtm.password == user.password:
+        raise HTTPException(status_code=401, detail="Invalid password")
+    return {
+        "message": "Login successful",
+        "user": {
+            "email": smtm.email,
+            "name": smtm.name
+        }
+    }
 
 
